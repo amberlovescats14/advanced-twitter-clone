0a81db697f8f8cbb0f0080435e96f63cc59feb5a
Amber Jones <amberlovescats14@gmail.com>
2019-05-07 19:19:32 -0500

    got components working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import  './App.css';
+import LeftComponent from'./components/LeftComponent';
+import RightComponent from './components/RightComponent';
+import MiddleComponent from './components/MiddleComponent';
+// import { tsConstructorType } from '@babel/types';
+
 
 function App(){
+  
    return (
     <div>
-
+    <RightComponent></RightComponent>
+    <h1>test</h1>
     </div>
   );
  }
---
+++ src/components/RightComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RightComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>AMber</h1>
+      </div>
+    )
+  }
+}
