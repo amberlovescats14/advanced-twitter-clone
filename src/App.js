7031041e261af64c1a01961911e865f4b47bc83e
Amber Jones <amberlovescats14@gmail.com>
2019-05-08 11:25:32 -0500

    error message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import Left1 from'./components/left-1/Left-1';
-import Right1 from './components/right-1/Right-1';
-import Middle1 from './components/middle-1/Middle-1';
-import Left2 from'./components/left-2/Left-2';
-import Right2 from './components/right-2/Right-2';
-import Middle2 from './components/middle-2/Middle-2';
+import Left1 from "./components/left1/Left1";
+import Right1 from "./components/right1/Right1";
+import Middle1 from "./components/middle1/Middle1";
+import Left2 from"./components/left2/Left2";
+import Right2 from './components/right2/Right2';
+import Middle2 from './components/middle2/Middle2';
 import Nav from './components/nav/Nav';
 
 
 import './css/main.css'
-// import { tsConstructorType } from '@babel/types';
+import { tsConstructorType } from '@babel/types';
 
 
 function App(){
