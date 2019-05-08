974ef245871b4fda091a2ba4f9fc052932f7a11d
Amber Jones <amberlovescats14@gmail.com>
2019-05-08 10:12:42 -0500

    added files and folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import LeftComponent from'./components/LeftComponent';
-import RightComponent from './components/RightComponent';
-import MiddleComponent from './components/MiddleComponent';
+import Left1 from'./components/left-1/Left-1';
+import Right1 from './components/right-1/Right-1';
+import Middle1 from './components/middle-1/Middle-1';
+import Left2 from'./components/left-2/Left-2';
+import Right2 from './components/right-2/Right-2';
+import Middle2 from './components/middle-2/Middle-2';
+import Nav from './components/nav/Nav';
+
+
 import './css/main.css'
 // import { tsConstructorType } from '@babel/types';
 
@@ -9,9 +15,15 @@ import './css/main.css'
 function App(){
   
    return (
-    <div>
-    <RightComponent></RightComponent>
-    <h1>test</h1>
+    <div id="main" className="cell-14">
+    <div id="one"><Nav></Nav></div>
+    <div id="two"><Left1></Left1></div>
+    <div id="three"><Left2></Left2></div>
+    <div id="four"><Middle1></Middle1></div>
+    <div id="five"><Middle2></Middle2></div>
+    <div id="six"><Right1></Right1></div>
+    <div id="seven"><Right2></Right2></div>
+    
     </div>
   );
  }
--- src/components/right-1/Right-1.js
+++ src/components/right-1/Right-1.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class RightComponent extends Component {
   render() {
     return (
-      <div>
-        <h1>AMber</h1>
-      </div>
+     <div>
+       <h1>amber</h1>
+     </div>
+     
+    
     )
   }
 }
