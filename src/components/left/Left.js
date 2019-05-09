082a5be1377bb8782b4092caf1395d2bfa9cd224
Amber Jones <amberlovescats14@gmail.com>
2019-05-09 17:14:17 -0500

    left top

--- src/components/left/Left.js
+++ src/components/left/Left.js
@@ -4,8 +4,46 @@ export default class Left extends Component {
   render() {
     return (
       <div id="left">
-      <div className="leftTop">top</div>
-      <div className="leftBottom">bottom</div>
+      <div className="leftTop">
+
+      <div id="content">
+      <div className="whitePortion"></div>
+      <section></section>
+      <img src="https://www.shutterbug.com/images/styles/600_wide/public/Promobl6618.jpg" alt="profilePic"></img>
+      <p className="name">
+      Jane Doe <br></br>
+      <small>@JaneDoe87 </small>
+      </p>
+      
+      <div className="bar">
+      <div className="flex">
+      <div> TWEETS <br></br>
+      <p>123</p>
+      </div>
+      <div> FOLLOWING <br></br>
+      <p>100</p>
+      </div>
+      <div> FOLLOWERS <br></br>
+      <p>26</p>
+      </div>
+      </div>
+      </div>
+      <div className="whiteBottom">
+      <div className="content">
+      <p className="bold">Gain more followers</p>
+      <p>Promote your account and get discovered by more people on Twitter</p>
+      <p><small>Preview it first below.</small></p>
+      </div>
+      </div>
+      </div>
+      </div>
+
+
+      <div className="leftBottom">
+      <div id="content2">
+      <h2>Trends</h2>
+      </div>
+      </div>
     </div>
     )
   }
