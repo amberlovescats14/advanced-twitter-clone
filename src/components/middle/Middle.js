a40799a620714ca08115279c2548eb2f47b95f90
Amber Jones <amberlovescats14@gmail.com>
2019-05-30 17:11:47 -0500

    done with sass and scss grid functions

--- src/components/middle/Middle.js
+++ src/components/middle/Middle.js
@@ -4,7 +4,22 @@ export default class Middle extends Component {
   render() {
     return (
       <div id="middle">
-        <div className="middleTop">middleTop</div>
+        <div className="middleTop">
+        <div className="flexTop">
+        <img src="https://www.shutterbug.com/images/styles/600_wide/public/Promobl6618.jpg" alt="profilePic"></img>
+        <section id="searchBar">
+            <input type="text" placeholder="What's Happening" id="barMiddle"></input>
+            <button type="submit" id="middleButton">
+            <i className="fas fa-camera fa-2x"></i>
+            </button>
+          </section>
+        </div>
+        </div>
+
+
+
+
+
         <div className="middleBottom">middleBottom</div>
       </div>
     )
